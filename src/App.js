4f95eb3ec6fc4874031e2c136e5a701cbbf00f13
Tyler Duran <tymduran@gmail.com>
2022-05-10 13:37:03 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [pokemon, setPokemon] = useState([])
+
+  const fetchPokemon = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+    .then(someResponse => {
+      return someResponse.json();
+    }).then(anotherResponse => {
+      console.log(anotherResponse.results);
+      // set my state variable here
+      setPokemon(anotherResponse.results);
+    }).catch( someErr => console.log(someErr))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Pokemon API</h1>
+      <button onClick={fetchPokemon}>Fetch Pokemon</button>
+      <hr />
+      {/* pokemon: {JSON.stringify(pokemon)} */}
+      <div>
+        {
+          pokemon.map( (poke, idx) => {
+            return (
+              <ul key={idx}>
+                <li>Name: {poke.name}</li>
+              </ul>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
+
 
 export default App;
